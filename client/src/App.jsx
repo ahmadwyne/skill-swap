bb7169b2d2ea151a35c7ec40b6a1f58800a71431
ahmadwyne <ahmad.wyne748@gmail.com>
2025-04-21 02:20:22 +0500

    Initial setup: MERN stack, React, Redux, Tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white h-screen flex justify-center items-center">
+      <h1 className="text-4xl font-bold">Welcome to SkillSwap!</h1>
+    </div>
+  );
+}
+
+export default App;
